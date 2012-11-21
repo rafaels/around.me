78b3f5653abdb1b5f8cab6bba2308cfc15486034
Rafael Souza <rafael@maisweb.org>
2012-11-21 20:28:59 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AroundMe::Application.config.secret_token = '6c3672bd781aa8733f2a69dfbef970ec7459887d5000efe291dacf00b93af4716d3091ff8beee540c5578fd47c205b46a0e7dc2990dffd154b921f77c1ebc01f'
