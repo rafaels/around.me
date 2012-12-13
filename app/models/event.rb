384ae3669af20a20b4e73dbefa2ff868e8c55d85
Rafael Souza <rafael@maisweb.org>
2012-12-13 08:11:51 -0300

    notificando novos eventos

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,7 +14,20 @@ class Event
   validates_presence_of :name, :description, :date, :x, :y, :created_by_id
 
   def participate!(new_user)
-    users << new_users
+    users << new_user
+  end
+
+  after_create :send_notifications
+
+  private
+  def send_notifications
+    sender = HiGCM::Sender.new("AIzaSyCYnvhAtPwzsz1fKUChyLbfeNOZuSpnMes")
+    reg_ids = (User.all - [created_by]).map {|user| user.reg_id}.compact
+    opts = {
+      :collapse_key => "new_event",
+      :data         => { :event_id => id }
+    }
+    response = sender.send(reg_ids, opts)
   end
 end
 
